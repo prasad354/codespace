aa6d19eb29b0e6ee73efc85c0637f2903061a3f6
prasad354 <prasad354@gmail.com>
2024-06-02 17:12:14 +0530

    Create playback speed.py

---
+++ playback speed.py
@@ -0,0 +1,7 @@
+def main():
+    user_input = input("Enter a string: ")
+    replaced_input = user_input.replace(" ", "...")
+    print("Input with spaces replaced:", replaced_input)
+
+if __name__ == "__main__":
+    main()
