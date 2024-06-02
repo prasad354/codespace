9e2b092870e4b6f0d7657c127b59182aa73ce8b6
prasad354 <prasad354@gmail.com>
2024-06-02 17:08:15 +0530

    Create indoor voice.py

---
+++ indoor voice.py
@@ -0,0 +1,7 @@
+def main():
+    user_input = input("Enter a string: ")
+    lowercase_input = user_input.lower()
+    print("Input in lowercase:", lowercase_input)
+
+if __name__ == "__main__":
+    main()
